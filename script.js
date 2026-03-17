c3b308b36ecbb762cea661ac4666c08b1d1048d9
sahilkhan86306145-lgtm <sahilkhan86306145@gmail.com>
2026-03-17 06:46:04 +0530

    Add animated counters and interactive JavaScript for billionaire website

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Website loaded");
+document.querySelectorAll("[data-count]").forEach(counter=>{
+
+let target=+counter.dataset.count
+let count=0
+
+function update(){
+
+count+=target/200
+
+if(count<target){
+
+counter.innerText=Math.floor(count).toLocaleString()
+
+requestAnimationFrame(update)
+
+}
+
+else{
+
+counter.innerText=target.toLocaleString()+"+"
+
+}
+
+}
+
+update()
+
+})
